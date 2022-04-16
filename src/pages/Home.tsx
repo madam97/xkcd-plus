6cc282980f383e2f60e0ee66a51cd264ea9a7e1b
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-16 14:48:18 +0200

    added comics reload button, lazy load to images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,24 +13,55 @@ export default function Home() {
 
   useEffect(() => {
     if (comicsStatus === 'idle') {
-      dispatch(fetchComics());
+      setTimeout(() => {
+        dispatch(fetchComics());
+      }, 2000);
     }
   }, [comicsStatus, dispatch]);
 
+  /**
+   * Reloads the 9 comics
+   * @param e 
+   */
+  const reloadComics = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+
+    dispatch(fetchComics());
+  }
+
   // ---------------------------------------
+
+  const imgPlaceholders: React.ReactNode[] = [];
+  for (let i = 0; i < 9; ++i) {
+    imgPlaceholders.push((
+      <div key={i}>
+        <div className="img-placeholder"></div>
+      </div>
+    ));
+  }
 
   return (
     <main>
+      <div className="loader"></div>
+
       <section className="container">
-        <Masonry>
+        <Masonry lazyLoad={true}>
+          {!comics && imgPlaceholders}
+
           {comics && comics.map(comic => 
             <div key={comic.num}>
-              <div className="img">
-                <img src={comic.img} alt={comic.alt} title={comic.title} />
+              <div className="img img-lozad">
+                <img data-src={comic.img} alt={comic.alt} title={comic.title} />
               </div>
             </div>
           )}
         </Masonry>
+
+        <div className="text-center">
+          <button className="btn-primary" onClick={reloadComics}>
+            Reload
+          </button>
+        </div>
       </section>
     </main>
   )
