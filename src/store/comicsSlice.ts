e93911cd3222e26d4c5ea38f3cfc3dda3dbd85fa
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-17 16:42:56 +0200

    fixed fetchComics to throw error, getComicsError gives back the error message not the status

--- src/store/comicsSlice.ts
+++ src/store/comicsSlice.ts
@@ -5,14 +5,10 @@ import IComic from '../interfaces/IComic';
 
 export const COMICS_URL = process.env.REACT_APP_API_BASE_URL + '/comics';
 
-export const fetchComics = createAsyncThunk('comics/random', async (): Promise<IComic[] | string> => {
-  try {
-    const res = await axios.get<IComic[]>(COMICS_URL + '/random');
+export const fetchComics = createAsyncThunk('comics/random', async (): Promise<IComic[]> => {
+  const res = await axios.get<IComic[]>(COMICS_URL + '/random');
 
-    return res.data;
-  } catch (err) {
-    return err instanceof Error ? err.message : 'unknown error';
-  }
+  return res.data;
 });
 
 
@@ -41,17 +37,17 @@ export const comicsSlice = createSlice({
       })
       .addCase(fetchComics.fulfilled, (state, action) => {
         state.status = 'ok';
-        state.list = typeof action.payload !== 'string' ? action.payload : [];
+        state.list = action.payload;
       })
       .addCase(fetchComics.rejected, (state, action) => {
         state.status = 'fail';
-        state.error = action.error.message;
+        state.error = 'comics/random error: ' + action.error.message;
       })
   }
 });
 
 export const selectComics = (state: RootState) => state.comics.list;
 export const getComicsStatus = (state: RootState) => state.comics.status;
-export const getComicsError = (state: RootState) => state.comics.status;
+export const getComicsError = (state: RootState) => state.comics.error;
 
 export default comicsSlice.reducer;
