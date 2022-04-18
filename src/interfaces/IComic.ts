e020b03b354ada570b89b23268ba9f6f0d6add37
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-18 12:16:35 +0200

    added docs comments to interfaces

--- src/interfaces/IComic.ts
+++ src/interfaces/IComic.ts
@@ -1,9 +1,16 @@
+/** IComic */
 interface IComic {
+  /** Number of the comic */
   num: number,
+  /** Source url of the comic's image */
   img: string,
+  /** Alt text of the image */
   alt: string,
+  /** Title of the image */
   title: string,
+  /** Transcript of the comic */
   transcript: string | null,
+  /** The upload date (UTC+0) of the comic */
   date: Date
 }
 
--- src/interfaces/IImage.ts
+++ src/interfaces/IImage.ts
@@ -1,7 +1,12 @@
+/** IImage */
 interface IImage {
+  /** Source url or path of the image */
   src: string,
+  /** Alt text of the image */
   alt: string,
+  /** Title of the image */
   title: string,
+  /** Url that loads after clicking on the image */
   link?: string
 }
 
