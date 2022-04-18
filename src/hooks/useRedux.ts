331ed8e6fb4f3da4fbcaae9b88323412eba9b192
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-18 12:16:18 +0200

    added docs comments to useRedux custom hooks and comics store

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,6 +1,20 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../store';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+/**
+ * The hook to use instead of plain `useDispatch`
+ * @category Custom hook
+ * @function
+ * @returns {AppDispatch}
+ */
+const useAppDispatch = () => useDispatch<AppDispatch>();
+
+/**
+ * The hook to use instead of plain `useSelector`
+ * @category Custom hook
+ * @function
+ * @returns {react-redux.TypedUseSelectorHook<RootState>}
+ */
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { useAppDispatch, useAppSelector };
--- src/store/comicsSlice.ts
+++ src/store/comicsSlice.ts
@@ -3,23 +3,41 @@ import axios from 'axios';
 import { RootState } from '../store';
 import IComic from '../interfaces/IComic';
 
-export const COMICS_URL = process.env.REACT_APP_API_BASE_URL + '/comics';
+/** 
+ * The full url to the /comics endpoint
+ * @category Store - Comics
+ * @const {string}
+ */
+const COMICS_URL = process.env.REACT_APP_API_BASE_URL + '/comics';
 
-export const fetchComics = createAsyncThunk('comics/random', async (): Promise<IComic[]> => {
+/**
+ * Fetching the /comics/random endpoint and return the list of comics
+ * @category Store - Comics
+ * @function
+ * @async
+ * @returns {Promise<IComic[]>}
+ */
+const fetchComics = createAsyncThunk('comics/random', async (): Promise<IComic[]> => {
   const res = await axios.get<IComic[]>(COMICS_URL + '/random');
 
   return res.data;
 });
 
 
-
+/** 
+ * ComicsState
+ * @category Store - Comics
+ */
 interface ComicsState {
+  /** The status of the last ran /comics endpoint fetch */
   status: 'idle' | 'ok' | 'fail' | 'loading',
+  /** The error message of the last ran /comics endpoint fetch */
   error?: string,
+  /** The result of the last ran /comics endpoint fetch*/
   list: IComic[]
 }
 
-export const comicsSlice = createSlice({
+const comicsSlice = createSlice({
   name: 'comics',
 
   initialState: {
@@ -46,8 +64,33 @@ export const comicsSlice = createSlice({
   }
 });
 
-export const selectComics = (state: RootState) => state.comics.list;
-export const getComicsStatus = (state: RootState) => state.comics.status;
-export const getComicsError = (state: RootState) => state.comics.error;
+/**
+ * Returns the list of fetched comics
+ * @category Store - Comics
+ * @function
+ * @param state 
+ * @returns {IComic[]}
+ */
+const selectComics = (state: RootState) => state.comics.list;
+
+/**
+ * Returns the status of the last comics fetch
+ * @category Store - Comics
+ * @function
+ * @param state 
+ * @returns {string}
+ */
+const getComicsStatus = (state: RootState) => state.comics.status;
+
+/**
+ * Returns the error message of the last comics fetch
+ * @category Store - Comics
+ * @function
+ * @param state 
+ * @returns {string | undefined}
+ */
+const getComicsError = (state: RootState) => state.comics.error;
+
+export { COMICS_URL, fetchComics, comicsSlice, selectComics, getComicsStatus, getComicsError };
 
 export default comicsSlice.reducer;
